248f9261a703e68eeee3c8445db786eef17565aa
teammy <teammy.teammy@gmail.com>
2024-05-19 22:59:36 +0700

    chore: Update npm dependencies and add react-hook-form and @hookform/resolvers

--- src/app/(frontend)/about/page.tsx
+++ src/app/(frontend)/about/page.tsx
@@ -1,3 +1,49 @@
+'use client'
+import Button from '@mui/material/Button'
+import { useForm, type SubmitHandler } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+import CustomTextField from '@/@core/components/mui/TextField'
+
+interface Inputs {
+  username: string
+  password: string
+}
+
+type FormProps = {
+  username: string
+  password: string
+}
+
 export default function Page() {
-  return <h1>About page!</h1>
+  const onSubmit = (data: Inputs) => {
+    console.log(data) // Handle form submission
+  }
+
+  const form = useForm<Inputs>({
+    defaultValues: {
+      username: '',
+      password: ''
+    }
+  })
+
+  // const handleSubmit = (event: any) => {
+  //   // Handle form submission
+  //   event.preventDefault()
+  //   console.log('event', event)
+
+  //   // Form submission logic
+  // }
+
+  return (
+    <div>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <CustomTextField label='username' {...form.register('username')} />
+        <CustomTextField label='password' type='password' {...form.register('password')} />
+        <Button variant='contained' type='submit'>
+          Primardddy
+        </Button>
+      </form>
+    </div>
+  )
 }
